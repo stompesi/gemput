f64a4ff4fa4cb82d5daf30f31ae821cb862408cc
stompesi <stompesi@gmail.com>
2015-04-14 21:31:12 +0900

    add help function.

--- lib/gemput.rb
+++ lib/gemput.rb
@@ -10,19 +10,19 @@ module Gemput
     unless gem_info['name'].nil?
       open('Gemfile', 'a') { |f|
         f.puts "gem '#{gem_info['name']}', '~> #{gem_info['version']}'"
-        "Add gem '#{gem_info['name']}', '~> #{gem_info['version']}'"
+        p "'#{gem_info['name']}' gem added."
       }
     else
-      "This rubygem could not be found."
+      p "This rubygem could not be found."
     end
   end
 
   def self.sync_gem()
-    return "Could not locate Gemfile." unless File.exists?('Gemfile')
-    read_text = File.open('Gemfile').read
+    return "Could not locate Gemfile." unless File.exists?('./Gemfile')
+    read_text = File.open('./Gemfile').read
     write_text = ''
 
-    p 'version check.'
+    p 'gem version synchronizing start.'
     read_text.each_line do |line|
       gem_line = /^\s*gem '([^']*)'\s*$/.match(line)
       unless gem_line.nil?
@@ -30,7 +30,7 @@ module Gemput
         gem_info = Gems.info(name)
         gem_version = gem_info['version']
         write_text << line.delete!("\n") << ", '~> " << gem_version << "'\n"
-        p "Add gem version '#{name}'"
+        p "'#{name}' gem version added."
       else
         write_text << line
       end
@@ -40,9 +40,29 @@ module Gemput
     if aFile
         aFile.syswrite(write_text)
     else
-        puts 'Unable open file'
+        p 'Unable open file.'
     end
 
-    p 'version check end.'
+    p 'gem version synchronizing finished.'
+  end
+
+  def self.show_help()
+    p 'This gem is gemversion management.'
+
+    p '# you can add gem with gem version in Gemfile.'
+    p '1-1 gemput add GEM_NAME'
+    p '1-2 gemput a GEM_NAME'
+    p '1-3 gemput -a GEM_NAME'
+    p ''
+
+    p '# you can fill gem version in Gemfile.'
+    p '2-1 gemput sync'
+    p '2-2 gemput s'
+    p '2-3 gemput -s'
+
+    p '# you can see help.'
+    p '2-1 gemput help'
+    p '2-2 gemput h'
+    p '2-3 gemput -h'
   end
 end
